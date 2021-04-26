0756fb6bee163a9ccb9129ffc673a08e4f7ec68d
Mateusz Lisowski <matilisek16@wp.pl>
2021-04-26 23:54:02 +0200

    Dodaj pliki projektów.

---
+++ Middleware/BrowserMiddlewareExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public static class BrowserMiddlewareExtensions
+    {     
+            public static IApplicationBuilder UseNowyMiddleware(this IApplicationBuilder builder)
+            {
+
+                return builder.UseMiddleware<NowyMiddleware>();
+            }
+    }
+}
+
+
+
---
+++ Middleware/NowyMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class NowyMiddleware
+    {
+        private RequestDelegate _next;
+        public NowyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var przegladarka = context.Request.Headers["User-Agent"];
+            var szukaj = przegladarka.ToString().ToLower().Contains("edg") || przegladarka.ToString().ToLower().Contains("trident");
+
+            if (szukaj)
+            {
+                context.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+            return _next(context);
+
+        }
+    }
+}
+
+   
